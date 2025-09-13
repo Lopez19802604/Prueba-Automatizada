107ec02cd8eb6af62589d1d439a84554220e92cf
Brandon Lopez <blopezv13@miumg.edu.gt>
2025-09-12 21:48:38 -0600

    test: check Google homepage

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('homepage has title', async ({ page }) => {
-  await page.goto('https://example.com/');
-  await expect(page).toHaveTitle(/Example Domain/i);
+test('Google homepage has title', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await expect(page).toHaveTitle(/Google/);
 });
